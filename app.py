e28702abdd8e29d8075fd231a3e4310f38fc72b6
Rangga52223 <rangga52223@gmail.com>
2025-11-16 14:42:39 +0700

    add cors middleware

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from route.test_route import test
 from route.need_route import need
 from route.serve_route import serve
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 app = FastAPI(
     title="HTH Apps API",
@@ -16,6 +17,21 @@ app.include_router(test)
 app.include_router(need)
 app.include_router(serve)
 
+origins = [
+    "*", # Izinkan akses dari semua origin. Paling mudah untuk development lokal.
+    # Jika ingin lebih spesifik (port Flutter default sering di atas 50000)
+    # "http://localhost:50000",
+    # "http://127.0.0.1:50000",
+]
+
+# 3. Tambahkan Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,           # Mengizinkan header cookie
+    allow_methods=["*"],              # Mengizinkan semua metode HTTP (GET, POST, PUT, DELETE)
+    allow_headers=["*"],              # Mengizinkan semua header dalam permintaan
+)
 
 if __name__ == "__main__":
     uvicorn.run('app:app', host="0.0.0.0", port=8000, reload=True)
